455002ff401a4de45c9612505db3d6e75bf8f369
Logan Davis <logan.davis@goteleport.com>
2021-12-31 11:15:45 -0600

    updates

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/logand22/xds-control-plane
+module github.com/logand22/xds-control-plane/v2
 
 go 1.16
--- pkg/output/output.go
+++ pkg/output/output.go
@@ -5,3 +5,7 @@ import "fmt"
 func Output(msg string) {
   fmt.Println(msg)
 }
+
+func Output2() {
+  fmt.Println("OUTPUT2")
+}
